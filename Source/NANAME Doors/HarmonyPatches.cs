8ef21acee6a5a995e2b998795faa29ef046588ea
oelsart <oelsart@gmail.com>
2026-02-26 19:58:20 +0900

    ReplaceContextMenuとの競合を解消

--- Source/NANAME Doors/HarmonyPatches.cs
+++ Source/NANAME Doors/HarmonyPatches.cs
@@ -186,3 +186,32 @@ public static class Patch_Designator_Dropdown_SetupFloatMenu
         return false;
     }
 }
+
+[HarmonyPatch("NoCrowdedContextMenu.Utilities.MenuOptionUtility", "OnBuildingPickerCreated")]
+public static class Patch_MenuOptionUtility_OnBuildingPickerCreated
+{
+    private static bool Prepare() => ModCompat.ReplaceContextMenu.Active;
+    
+    public static void Prefix(List<Designator> buildings)
+    {
+        if (buildings.ElementAtOrDefault(0) is not Designator_Build designator_Build ||
+            buildings.ElementAtOrDefault(1) is not Designator_Build designator_Build2 ||
+            designator_Build.PlacingDef is not ThingDef { MadeFromStuff: true } thingDef ||
+            designator_Build2.PlacingDef is not ThingDef { MadeFromStuff: true } thingDef2 ||
+            !NanameDoors.Mod.nanameDoors.ContainsKey(thingDef) ||
+            !NanameDoors.Mod.nanameDoors.ContainsValue(thingDef2)) return;
+
+        var count = designator_Build.Map.resourceCounter.AllCountedAmounts.Keys
+            .Count(item => !item.IsStuff || !item.stuffProps.CanMake(thingDef) || (!DebugSettings.godMode &&
+                designator_Build.Map.listerThings.ThingsOfDef(item).Count <= 0));
+
+        for (var i = 0; i < count - 1; i++)
+        {
+            buildings.Insert(0, designator_Build);
+        }
+        for (var i = 0; i < count - 1; i++)
+        {
+            buildings.Add(designator_Build2);
+        }
+    }
+}
--- Source/NANAME Doors/ModCompat.cs
+++ Source/NANAME Doors/ModCompat.cs
@@ -15,4 +15,11 @@ public static class ModCompat
     {
         public static readonly bool Active = ModsConfig.IsActive("cedaro.material.submenu") || ModsConfig.IsActive("WSP.GroupedBuildings");
     }
+
+    public static class ReplaceContextMenu
+    {
+        public static readonly bool Active = ModsConfig.IsActive("Nebulae.NoCrowdedContextMenu");
+
+        public const string PatchCategory = "Patches_ReplaceContextMenu";
+    }
 }
